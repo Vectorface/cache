d461020ceebc356a7fb243c211f1411862f0f7f2
Jonathan Anderson <janderson@vectorface.com>
2015-06-26 15:34:25 -0400

    Re-factor CacheHelper slightly, and add better phpdoc.

--- src/CacheHelper.php
+++ src/CacheHelper.php
@@ -23,6 +23,13 @@ class CacheHelper
      *     $cache = new APCCache();
      *     return CacheHelper::fetch($cache, $key, [SomeClass, 'getLargeDatasetFromDB'], [$arg1, $arg2], 600);
      * }
+     *
+     * @param Cache $cache The cache from/to which the values should be retrieved/set.
+     * @param string $key The cache key which should store the value.
+     * @param callable $callback A callable which is expected to return a value to be cached.
+     * @param mixed[] $args The arguments to be passed to the callback, if it needs to be called.
+     * @param int $ttl If a value is to be set in the cache, set this expiry time (in seconds).
+     * @return mixed The value stored in the cache, or returned by the callback.
      */
     public static function fetch(Cache $cache, $key, $callback, $args, $ttl = 300)
     {
@@ -32,15 +39,27 @@ class CacheHelper
 
         $item = $cache->get($key);
         if ($item === false) {
-            if (!is_array($args)) {
-                $args = isset($args) ? array($args) : array();
-            }
-            $item = call_user_func_array($callback, $args);
+            $item = static::runCallback($callback, $args);
 
             if (isset($item)) {
                 $cache->set($key, $item, $ttl);
             }
         }
         return $item;
+    }
+
+    /**
+     * Run the callback, normalizing the arguments.
+     *
+     * @param callable $callback The callable to be executed to fetch the value the cache.
+     * @param mixed[] $args The argument(s) to the callback function.
+     * @return mixed The value returned by the callback.
+     */
+    protected static function runCallback($callback, $args)
+    {
+        if (!is_array($args)) {
+            $args = isset($args) ? array($args) : array();
+        }
+        return call_user_func_array($callback, $args);
     }
 }
