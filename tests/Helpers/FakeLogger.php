07b8f3db5b388d1e03e4715bbaa4784e04516c3b
Francis Lavoie <lavofr@gmail.com>
2024-01-21 18:18:30 -0500

    Fix psr/log dependency

--- tests/Helpers/FakeLogger.php
+++ tests/Helpers/FakeLogger.php
@@ -9,47 +9,56 @@ class FakeLogger implements LoggerInterface
 {
     private $lastMessage;
 
-    public function emergency($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function emergency($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function alert($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function alert($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function critical($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function critical($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function error($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function error($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function warning($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function warning($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function notice($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function notice($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function info($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function info($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function debug($message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function debug($message, array $context = [])
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function log($level, $message, array $context = array())
+    #[\ReturnTypeWillChange]
+    public function log($level, $message, array $context = [])
     {
         $this->lastMessage = sprintf(
             "%s: %s%s",
