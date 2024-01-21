84d05b5b18e78d7eaead930b4dea272737851a1c
Francis Lavoie <lavofr@gmail.com>
2024-01-21 18:43:18 -0500

    Fix psr/log dependency, bump to PHP 8.0 minimum (#12)

--- tests/Helpers/FakeLogger.php
+++ tests/Helpers/FakeLogger.php
@@ -4,52 +4,53 @@
 namespace Vectorface\Tests\Cache\Helpers;
 
 use Psr\Log\LoggerInterface;
+use Stringable;
 
 class FakeLogger implements LoggerInterface
 {
     private $lastMessage;
 
-    public function emergency($message, array $context = array())
+    public function emergency(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function alert($message, array $context = array())
+    public function alert(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function critical($message, array $context = array())
+    public function critical(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function error($message, array $context = array())
+    public function error(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function warning($message, array $context = array())
+    public function warning(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function notice($message, array $context = array())
+    public function notice(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function info($message, array $context = array())
+    public function info(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function debug($message, array $context = array())
+    public function debug(Stringable|string $message, array $context = []): void
     {
         $this->log(__FUNCTION__, $message, $context);
     }
 
-    public function log($level, $message, array $context = array())
+    public function log($level, $message, array $context = []): void
     {
         $this->lastMessage = sprintf(
             "%s: %s%s",
