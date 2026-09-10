dbc983af3a24d2bde3f06794baa7840e8c389e54
Jonathan Anderson <janderson@vectorface.com>
2026-09-10 14:28:32 -0400

    Drop APC support from APCCache, use APCu directly
    APC is long deprecated in favour of APCu. Remove the module-name
    indirection and call the apcu_* functions directly.

--- tests/APCCacheTest.php
+++ tests/APCCacheTest.php
@@ -2,8 +2,6 @@
 
 namespace Vectorface\Tests\Cache;
 
-use Exception;
-use ReflectionClass;
 use Vectorface\Cache\APCCache;
 
 class APCCacheTest extends GenericCacheTest
@@ -14,23 +12,5 @@ class APCCacheTest extends GenericCacheTest
             $this->markTestSkipped("APCu module not loaded, or not enabled");
         }
         $this->cache = new APCCache();
-    }
-
-    public function testModuleNotAvailable()
-    {
-        $cls = new ReflectionClass(APCCache::class);
-        $prop = $cls->getProperty('apcModule');
-        $prop->setAccessible(true);
-        try {
-            $cache = new APCCache();
-            $orig = $prop->getValue($cache);
-            $prop->setValue($cache, 'invalidmodulename');
-            $this->fail('The invalidated module name should prevent using this cache.');
-        } catch (Exception) {
-            // Expected.
-        }
-
-        /** @noinspection PhpUndefinedVariableInspection */
-        $prop->setValue($cache, $orig);
     }
 }
